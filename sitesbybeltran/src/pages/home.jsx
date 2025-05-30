910b3a1323888f8f8211580e5db282bf73283a0c
Angel Beltran <aabeltran5910@gmail.com>
2025-05-29 22:34:14 -0500

    Finished services page

--- sitesbybeltran/src/pages/home.jsx
+++ sitesbybeltran/src/pages/home.jsx
@@ -27,7 +27,7 @@ export default function Home() {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a 
-                href="#" 
+                href="/services" 
                 className="rounded-md bg-rose-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-rose-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Get started
